754ab060d7daf47ce79a506909e3b458bbdec153
Sebi <emanuel-sebastian.visan-sebastian@s.unibuc.ro>
2023-04-10 12:02:06 +0300

    Compunere adaugata

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <windows.h>
+//#include <rlutil.h>
 
 using namespace std;
 
@@ -24,41 +25,110 @@ public:
     static void win(){
         cout << "You are so sweet!";
     }
-    friend class Bee;
+    [[nodiscard]] bool request_purchase(const int price) const{
+        if(honey_jars >= price)
+            return true;
+        return false;
+    }
+    friend class Bees;
     friend class Hive;
 };
-//Future Compunere: class Bees care are un array de obiecte Bee
+
 class Bee{
-    int number, price;
-    double increment, profit;
+    int price;
+    double rate;
 public:
-    Bee(){
-       number = 0;
-       price = 10;
-       increment = 1.5;
-       profit = 0.1;
+    Bee(){ //constructor fara parametrii folosit la initializarea array-ului cu o albina lenesa
+       price = 0;
+       rate = 0;
+    }
+    Bee(int p){
+       rate = 0.1;  // once every 100ms - all bees have the same rate but this could change
+       price = p;
     }
     ~Bee() = default;
-    Bee(const Bee& other) : number{other.number}, price{other.price}, increment{other.increment}, profit{other.profit}
+    Bee(const Bee& other) : price{other.price}, rate{other.rate}
     {
         cout << "copy constructor";
     }
     friend ostream& operator <<(ostream& os, const Bee& b) {
-        return os << "Bees: " << b.number << "  |  (price) " << b.price << "\n";
+        return os << "This bee costed this much: " <<  b.price << "\n";
+    }
+    double produce() const{
+        if(rand() % 100 < 2)
+            return rate * 2; //fiecare albina are probabilitatea de 2% de a dubla productia
+        return rate;
+    }
+};
+class Bees{
+    int number, curent_price;
+    double increment, total_profit;
+    Bee* ob;
+public:
+    Bees(){
+        number = 0;
+        increment = 1.5;
+        total_profit = 0;
+        curent_price = 10;
+        ob = new Bee[1];
+        ob[0] = Bee();
+    }
+    ~Bees() {
+        delete[] ob;
+    }
+    Bees(const Bees &other): number{other.number}, curent_price{other.curent_price}, increment{other.increment}{
+        total_profit = 0;
+        ob = new Bee[number];
+        for(int i = 0; i < number; i++) {
+            ob[i] = other.ob[i];
+        }
+    }
+    Bees& operator=(const Bees& other){
+        if(this==&other)
+            return *this;
+
+        delete[] this->ob;
+        this->ob=new Bee[this->number=other.number];
+        for(int i=0;i<this->number;++i)
+            this->ob[i]=other.ob[i];
+
+        this->number = other.number;
+        this->curent_price = other.curent_price;
+        this->increment = other.increment;
+        this->total_profit = 0;
+
+        return *this;
     }
 
-    [[nodiscard]] bool request_purchase(const Wallet& w) const{
-        if(w.honey_jars >= price)
-            return true;
-        return false;
+    friend ostream& operator <<(ostream& os, const Bees& b) {
+        return os << "Bees: " << b.number << "  |  (price) " << b.curent_price << "\n";
+    }
+    void calculate_curent_price(){
+        curent_price = (int)(curent_price * increment);
     }
     void purchase(Wallet& w){
+        if(w.request_purchase(curent_price)){
+        w.honey_jars -= curent_price;
+
+        Bee* temp = new Bee[number + 1];
+        for(int i = 0; i < number; i++) {
+            temp[i] = ob[i];
+        }
+        temp[number] = Bee(curent_price);
         number++;
-        w.honey_jars -= price;
-        price = (int)(price * increment);
+        delete[] ob;
+        ob = temp;
+        calculate_curent_price();
+        }
     }
-    void produce(Wallet& w) const{
-        w.honey_jars += number * profit;
+    void calculate_profit(){
+        total_profit = 0;
+        for(int i = 0; i <= number; i++){
+            total_profit += ob[i].produce();
+        }
+    }
+    void increase(Wallet& w) const{
+        w.honey_jars += total_profit;
     }
 };
 
@@ -92,8 +162,8 @@ public:
     }
 };
 
-void show_screen(const Wallet& w, const Bee& b, const Hive& h){
-    system("cls"); //clears screen
+void show_screen(const Wallet& w, const Bees& b, const Hive& h){
+    system("cls"); //de reparat cu rlutil.h
     cout << "                          H o n e y   C l i c k e r            \n";
     cout << "[click] Left Click   |   [exit] Q   |   [bee] B   |   [hive] H \n \n";
     cout << w;
@@ -103,7 +173,7 @@ void show_screen(const Wallet& w, const Bee& b, const Hive& h){
 
 int main () {
     Wallet wall;
-    Bee bee;
+    Bees bees;
     Hive hive;
     Sleep(100);
 
@@ -112,16 +182,16 @@ int main () {
             wall.click(); //click detection
         }
         if (GetAsyncKeyState(0x42)) {
-            if (bee.request_purchase(wall))
-                bee.purchase(wall);
+                bees.purchase(wall);
         }
         if (GetAsyncKeyState(0x48)) {
             if (hive.request_purchase(wall))
                 hive.purchase(wall);
         }
-        bee.produce(wall);
+        bees.calculate_profit();
+        bees.increase(wall);
         hive.produce(wall);
-        show_screen(wall, bee, hive);
+        show_screen(wall, bees, hive);
         if(wall.check_win()){
             Wallet::win();
             return 0;
