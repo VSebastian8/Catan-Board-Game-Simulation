4593e32392d36017076ce0bde100e35eab203b3d
VSebastian8 <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2023-05-17 01:20:53 +0300

    Board

--- main.cpp
+++ main.cpp
@@ -1,54 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <rlutil.h>
 
-class Tile{
-protected:
-    std::string name, resource;
+#ifdef __linux__
+#include <X11/Xlib.h>
+#endif
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include "tiles.h"
+#include "board.h"
+
+
+class Game{
+    sf::Event e;
+    sf::VideoMode vm;
+    sf::RenderWindow* window;
+    Board b;
 public:
-    Tile() = default;
-    virtual ~Tile() = default;
-    friend std::ostream& operator <<(std::ostream& out, const Tile& tile){
-        tile.show();
-        return out;
-    }
-    virtual void show() const {
-        std::cout << name << " gives you: " << resource << "\n";
-    }
-};
-class Hill: public Tile{
-public:
-    Hill(){name = "Hill"; resource = "brick";}
-};
-class Field: public Tile{
-public:
-    Field(){name = "Field"; resource = "hay";}
-};
-class Pasture: public Tile{
-public:
-    Pasture(){name = "Pasture"; resource = "sheep";}
-};
-class Forest: virtual public Tile{
-public:
-    Forest(){name = "Forest"; resource = "wood";}
-};
-class Mountain: virtual public Tile{
-public:
-    Mountain(){name = "Mountain"; resource = "rock";}
-};
-class Rocky_Jungle: public Forest, public Mountain{
-    std::string resource2;
-public:
-    Rocky_Jungle(){name = "Rocky Jungle"; resource2 = "wood";}
-    void show() const override{
-        std::cout << name << " gives you: " << resource << " + " << resource2 << "\n";
+    Game(): e(sf::Event()), vm(sf::VideoMode(900, 900)),
+            window(new sf::RenderWindow(vm, "Catran", sf::Style::Close | sf::Style::Titlebar)),
+            b(Board()){}
+    void run(){
+        b.initialize();
+        while(window->isOpen()) {
+            while(window->pollEvent(e)){
+                switch(e.type)
+                {
+                    case sf::Event::Closed : window->close(); break;
+                    default: break;
+                }
+            }
+            window->clear();
+            b.show(window);
+            window->display();
+        }
     }
 };
+void info_console(){
+    rlutil::setColor(rlutil::YELLOW);
+    std::vector<std::shared_ptr<Tile>> info_tiles = {std::make_shared<Forest>(), std::make_shared<Hill>(), std::make_shared<Pasture>(),
+                                       std::make_shared<Field>(), std::make_shared<Mountain>(), std::make_shared<Rocky_Jungle>()};
+    for(const auto &i : info_tiles)
+        std::cout << *i;
+    rlutil::setColor(rlutil::WHITE);
+}
 
 int main () {
-    std::vector<std::shared_ptr<Tile>> tiles = {std::make_shared<Hill>(), std::make_shared<Field>(), std::make_shared<Pasture>(),
-            std::make_shared<Forest>(), std::make_shared<Mountain>(), std::make_shared<Rocky_Jungle>()};
-    for(const auto &i : tiles)
-        std::cout << *i;
+#ifdef __linux__
+    XInitThreads();
+#endif
+
+    info_console();
+    Game g;
+    g.run();
+
     return 0;
 }
